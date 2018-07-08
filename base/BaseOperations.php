fdecdcb7b6786e11065d32581bd731b37fd6df93
Dara <123dara@gmail.com>
2018-07-08 19:02:06 -0400

    Add return type

--- base/BaseOperations.php
+++ base/BaseOperations.php
@@ -9,33 +9,33 @@ interface BaseOperations
      *
      * @return Response
      */
-    public function authorize(Request $request);
+    public function authorize(Request $request): Response;
 
     /**
      * @param Request $request
      *
      * @return Response
      */
-    public function purchase(Request $request);
+    public function purchase(Request $request): Response;
 
     /**
      * @param Request $request
      *
      * @return Response
      */
-    public function void(Request $request);
+    public function void(Request $request): Response;
 
     /**
      * @param Request $request
      *
      * @return Response
      */
-    public function refund(Request $request);
+    public function refund(Request $request): Response;
 
     /**
      * @param Request $request
      *
      * @return Response
      */
-    public function capture(Request $request);
+    public function capture(Request $request): Response;
 }
